9de3e1d14753834019f72a0841fee1d22ced61b5
Vinicius Teixeira Colombo <vinicius.296922@alunosatc.edu.br>
2025-10-29 19:26:33 -0300

    testando

--- desafio02/index01.js
+++ desafio02/index01.js
@@ -1,1 +1,52 @@
-console.log("Hello, World! 001");
+// problemDecorator.js
+class Message {
+    constructor(text) {
+      this.text = text;
+    }
+  
+    getText() {
+      return this.text;
+    }
+  }
+  
+  class MessageDecorator {
+    constructor(message) {
+      this.message = message;
+    }
+  
+    getText() {
+      return this.message.getText();
+    }
+  }
+  
+  class UpperCaseDecorator extends MessageDecorator {
+    getText() {
+      return super.getText().toUpperCase();
+    }
+  }
+  
+  class PositiveMessageDecorator extends MessageDecorator {
+    getText() {
+      let text = super.getText();
+      return text.replace("horrível", "maravilhoso");
+    }
+  }
+  
+  class CensorDecorator extends MessageDecorator {
+    getText() {
+      let text = super.getText();
+      return text.replace("horrível", "h*******");
+    }
+  }
+  
+  const msg = new Message("hoje o dia está horrível");
+  console.log("Original:", msg.getText());
+  
+  const upperMsg = new UpperCaseDecorator(msg);
+  console.log("Uppercase:", upperMsg.getText());
+  
+  const positiveMsg = new PositiveMessageDecorator(msg);
+  console.log("Positivo:", positiveMsg.getText());
+  
+  const angryCensoredMsg = new UpperCaseDecorator(new CensorDecorator(msg));
+  console.log("Censurado e Maiúsculo:", angryCensoredMsg.getText());
--- desafio02/index02.js
+++ desafio02/index02.js
@@ -1,1 +1,47 @@
-console.log("Hello, World! 002");
+// problemAdapter.js
+
+class LegacyPaymentSystem {
+    makePayment(amount) {
+      console.log(`Pagando R$${amount} com sistema legado.`);
+    }
+  }
+  
+  class ModernPaymentAPI {
+    process(amountInCents) {
+      console.log(`Pagamento de R$${amountInCents / 100} via API moderna.`);
+    }
+  }
+  
+  class PaymentProcessor {
+    constructor(system) {
+      this.system = system;
+    }
+  
+    pay(amount) {
+      this.system.makePayment(amount);
+    }
+  }
+  
+  class ModernPaymentAdapter {
+    constructor(modernAPI) {
+      this.modernAPI = modernAPI;
+    }
+  
+    makePayment(amount) {
+      const amountInCents = amount * 100;
+      
+      this.modernAPI.process(amountInCents);
+    }
+  }
+  
+  const legacy = new LegacyPaymentSystem();
+  const processorLegacy = new PaymentProcessor(legacy);
+  console.log("--- Testando Sistema Legado ---");
+  processorLegacy.pay(100); //
+  
+  const modernAPI = new ModernPaymentAPI();
+  const adapter = new ModernPaymentAdapter(modernAPI); 
+  const processorModern = new PaymentProcessor(adapter); 
+  
+  console.log("\n--- Testando API Moderna com Adapter ---");
+  processorModern.pay(250);
